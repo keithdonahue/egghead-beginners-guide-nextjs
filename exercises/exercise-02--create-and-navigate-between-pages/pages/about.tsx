17dc08eb11aedf045809ae41e88844bf85f0aeac
Keith Donahue <bkshankz@gmail.com>
2024-02-14 01:54:40 +0000

    ok back on track

--- exercises/exercise-02--create-and-navigate-between-pages/pages/about.tsx
+++ exercises/exercise-02--create-and-navigate-between-pages/pages/about.tsx
@@ -1,13 +1,14 @@
-import { Link } from 'next/link'
+import Link from 'next/link'
 
 const About = () => {
   return (
     <div>
       <h1>Welcome to the About page!</h1>
       <Link href="/">Go back home</Link>
+      <br />
+      <a href="/">Go back to home page full refresh</a>
       {/* ✍️ put a link to the About page */}
     </div>
   )
 }
-
 export default About
--- exercises/exercise-02--create-and-navigate-between-pages/pages/index.tsx
+++ exercises/exercise-02--create-and-navigate-between-pages/pages/index.tsx
@@ -1,9 +1,11 @@
 // ✍️ import the Link component from 'next/link'
+import Link from 'next/link'
 
 const Home = () => {
   return (
     <div>
       <h1>Welcome to the Home page!</h1>
+      <Link href="/about">About</Link>
       {/* ✍️ put a link to the About page */}
     </div>
   )
