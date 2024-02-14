07769e5bcc7671e8d85cbe4956a45e3eefda99c7
Keith Donahue <bkshankz@gmail.com>
2024-02-14 18:25:09 +0000

    Complete lesson 5.

--- exercises/exercise-04--serve-static-assets/pages/index.tsx
+++ exercises/exercise-04--serve-static-assets/pages/index.tsx
@@ -3,6 +3,7 @@ const Home = () => {
     <div>
       <h2>Hello 👋</h2>
       <p>My name is Lazar Nikolov</p>
+      <img src="/lazar.png" alt="Lazar Nikolov" />
       {/* ✍️ add the "lazar.png" image here */}
     </div>
   )
--- exercises/exercise-05--use-next-image-for-image-optimization/pages/index.tsx
+++ exercises/exercise-05--use-next-image-for-image-optimization/pages/index.tsx
@@ -1,11 +1,23 @@
 // ✍️ import the Image component from 'next/image'
+import Image from 'next/image'
+
+import lazar from '../lazar.png'
+
+const worms = 'https://pbs.twimg.com/profile_images/454658481/us_400x400.png'
 
 // ✍️ import "lazar.png" statically
 
 const Home = () => {
-  return {
-    /* ✍️ add the lazar image here */
-  }
+  return (
+    <>
+      <Image src={lazar} alt="Lazar Nikolov" />
+      <Image src={lazar} alt="Lazar Nikolov" />
+      <Image src={lazar} alt="Lazar Nikolov" />
+      <Image src={lazar} alt="Lazar Nikolov" />
+      <Image src={worms} alt="worms" width={550} height={550} />
+      <Image src={lazar} alt="Lazar Nikolov" />
+    </>
+  )
 }
 
 export default Home
