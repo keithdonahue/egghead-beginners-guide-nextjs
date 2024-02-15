6e070377cb20179f647cb088d835ae26e43aaa1c
Keith Donahue <bkshankz@gmail.com>
2024-02-15 19:03:25 +0000

    working through exercise 10

--- exercises/exercise-10--create-custom-layouts/pages/_app.tsx
+++ exercises/exercise-10--create-custom-layouts/pages/_app.tsx
@@ -2,12 +2,15 @@ import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 
 // ✍️ import the custom layout
+import Layout from '../src/components/layout'
 
 const App = ({ Component, pageProps }: AppProps) => {
   // ✍️ wrap the Component with the custom layout
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ChakraProvider>
   )
 }
--- exercises/exercise-10--create-custom-layouts/src/components/layout/index.tsx
+++ exercises/exercise-10--create-custom-layouts/src/components/layout/index.tsx
@@ -1,12 +1,21 @@
 import type { ReactNode } from 'react'
 
+import Navbar from './navbar'
+import Footer from './footer'
+
 type Props = {
   children?: ReactNode
 }
 
 const Layout = ({ children }: Props) => {
   // ✍️ create the custom layout
-  return <></>
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+      <Footer />
+    </>
+  )
 }
 
 export default Layout
